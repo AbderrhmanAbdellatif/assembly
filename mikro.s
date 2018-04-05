6c811da8e3bc8182a817b101b5d796b8d8110869
AbderrhmanAbdellatif <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2018-04-05 20:54:29 +0300

    new

--- mikro.s
+++ mikro.s
@@ -5,11 +5,13 @@
 	;LDR r1,=0x80000000 ;1000
     ;ADDS r2,r0,r1      ;0100
     ;END
-	TTL	Ders1
-	AREA	Program, CODE, READONLY
-	ENTRY			
-	LDR r0,=2_101
-	LSL r1,r0,#7 
 	MOV r2,#0
-	END
+	;TTL	Ders1
+	;AREA	Program, CODE, READONLY
+	;ENTRY			
+	;LDR r0,=2_101 ;saga kaydirma
+	;LSL r1,r0,#7 ; 7 tane kaydirma
+	;MOV r2,#0 ; 
+	;END
+	
 	
